6505ee7de6255a130560873b89c98ce62bf6a0d7
Emmanuel Courcelle <emmanuel.courcelle@toulouse-inp.fr>
2020-09-25 15:44:28 +0200

    Common configurations for server/shibboleth and laptop

---
+++ roles/portal/templates/callisto/index-dataverse.php
@@ -0,0 +1,13 @@
+  <?php include "headers.inc"; ?>
+  <?php include "menu.inc"; ?>
+  
+  <!-- Dataverse frame -->
+
+  <div style="height:90%;margin:0;padding:0;">
+  <iframe width="100%" height="100%" sandbox="allow-scripts allow-forms allow-modals allow-popups allow-same-origin " src="https://dataverse.callisto-local.mylaptop/loginpage.xhtml"></iframe>
+  <!--<iframe width="100%" height="100%" sandbox="allow-scripts allow-forms allow-modals allow-popups allow-same-origin " src="https://dataverse.callisto-local.mylaptop/shib.xhtml"></iframe>-->
+  </div>
+
+
+  </body>
+</html>
