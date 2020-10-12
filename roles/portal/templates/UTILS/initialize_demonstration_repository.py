18f2d03da67c0ea0b63658035f9c694291bb0b29
Thierry <thierry.louge@toulouse-inp.fr>
2020-10-12 09:49:54 +0200

    Modif du path fichiers utils sous /usr/local/bin (Portal)

--- roles/portal/templates/UTILS/initialize_demonstration_repository.py
+++ roles/portal/templates/UTILS/initialize_demonstration_repository.py
@@ -8,7 +8,7 @@ mode = Repository.ACCESS
 repository = catalog.getRepository("demonstration",mode)
 repository.initialize()
 conn = repository.getConnection()
-conn.addFile("./demonstration.nt",format="application/n-triples")
+conn.addFile("/home/callisto/demonstration.nt",format="application/n-triples")
 labels = conn.createURI("<http://www.w3.org/2000/01/rdf-schema#label>")
 get_data = conn.createURI(arcas.get_dataset)
 conn.add(get_data,"<http://www.w3.org/2000/01/rdf-schema#label>",Literal("get_dataset.py"))
