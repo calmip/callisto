ca308a6cdf2289d1790ef94aa056d67b07968ea3
Thierry <thierry.louge@toulouse-inp.fr>
2020-10-12 08:47:36 +0200

    Modif pour fin de passage depot allegro fonctionnel

--- roles/portal/templates/UTILS/initialize_demonstration_repository.py
+++ roles/portal/templates/UTILS/initialize_demonstration_repository.py
@@ -1,5 +1,7 @@
 from franz.openrdf.sail.allegrographserver import AllegroGraphServer
 from franz.openrdf.repository.repository import Repository
+from rdflib import Namespace, URIRef, Literal
+arcas = Namespace("http://www.callisto.calmip.univ-toulouse.fr/ARCAS.rdf#")
 server=AllegroGraphServer(host="CallistoAllegro",port="{{allegro_port}}",user="{{allegro_user}}",password="{{allegro_password}}")
 catalog = server.openCatalog('')
 mode = Repository.ACCESS
@@ -7,3 +9,7 @@ repository = catalog.getRepository("demonstration",mode)
 repository.initialize()
 conn = repository.getConnection()
 conn.addFile("./demonstration.nt",format="application/n-triples")
+labels = conn.createURI("<http://www.w3.org/2000/01/rdf-schema#label>")
+get_data = conn.createURI(arcas.get_dataset)
+conn.add(get_data,"<http://www.w3.org/2000/01/rdf-schema#label>",Literal("get_dataset.py"))
+
