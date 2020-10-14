8f8bd6ec50363d04b81ae873f89e47fb81e34186
Emmanuel Courcelle <emmanuel.courcelle@toulouse-inp.fr>
2020-10-14 14:36:15 +0200

    Adding LICENSE information (AGPL license)

--- roles/portal/templates/callisto/index-tools.php
+++ roles/portal/templates/callisto/index-tools.php
@@ -1,3 +1,33 @@
+<?php
+/*
+ *
+ * This file is part of Callisto software
+ * Callisto helps scientists to share data between collaborators
+ * 
+ * Callisto is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ *  Copyright (C) 2015-2021    C A L M I P
+ *  callisto is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ *  You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+ *  along with Callisto.  If not, see <http://www.gnu.org/licenses/agpl-3.0.txt>.
+ *
+ *  Authors:
+ *        Thierry Louge      - C.N.R.S. - UMS 3667 - CALMIP
+ *        Emmanuel Courcelle - C.N.R.S. - UMS 3667 - CALMIP
+ *
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  
   <?php include "headers.inc"; ?>
   <?php include "menu.inc"; ?>
   
