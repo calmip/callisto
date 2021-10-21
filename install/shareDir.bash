82dcc96b9712d99d715c75b6801c05b6f2ed6dc4
Emmanuel Courcelle <emmanuel.courcelle@toulouse-inp.fr>
2021-10-21 15:28:21 +0200

    BUG - Corrected sharing directory code

--- install/shareDir.bash
+++ install/shareDir.bash
@@ -4,12 +4,14 @@
 printf "uid $(id -u) 0\ngid $(id -g) 0" | lxc config set CallistoPortal raw.idmap -
 
 # Create the share in CallistoPortal (we share the subdirectory callisto of the cloned tree) and restart the container:
-lxc config device add CallistoPortal CallistoPortal-disk disk source=$(pwd)/callisto path=/usr/local/callisto
+lxc config device add CallistoPortal CallistoPortal-disk disk source=$(cd .. && pwd)/callisto path=/usr/local/callisto
 lxc restart CallistoPortal
 
 # sleep 15s (should be enough for the container to restart)
 sleep 15
 
 # Create a file
-echo "THIS DIRECTORY IS SHARED BETWEEN THE HOST AND THE CONTAINER CallistoPortal" > ../callisto/SHARED.txt
+echo "THIS DIRECTORY IS SHARED BETWEEN THE HOST AND THE CONTAINER CallistoPortal\n" > ../callisto/.SHARED.txt
+echo "PLEASE REMOVE THIS FILE IF YOU WANT TO SHARE THE DIRECTORY AGAIN !\n" >> ../callisto/.SHARED.txt
+
 
